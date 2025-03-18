4717d56117b997407926a8f4f19c81bed7ee322f
Gabo Morales <gabrielmoralegui@gmail.com>
2025-03-18 18:47:17 -0500

    Subo el primer archivo

---
+++ Codigos/calcpccolegio.py
@@ -0,0 +1,7 @@
+num1 = int(input("Ingresa el primer número: "))
+num2 = int(input("Ingresa el segundo número: "))
+
+if num1 < num2:
+  print(num1,"es menor que",num2)
+else:
+  print(num1,"no es menor que",num2)
