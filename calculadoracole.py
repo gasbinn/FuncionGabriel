b2340e4dfeaddfe4a70956fe52632f7f787a977c
Cristian <cristianttrujillo@gmail.com>
2025-03-20 13:08:18 -0500

    Subo el archivo desde el computador del colegio, con añadido delcodigo para el fin del programa.

---
+++ calculadoracole.py
@@ -0,0 +1,8 @@
+n1 = int(input("Ingresa el primer número: "))
+n2 = int(input("Ingresa el segundo número: "))
+
+if n1 < n2:
+  print(n1,"es menor que",n2)
+else:
+  print(n1,"no es menor que",n2)
+print("Fin del programa del computador")
